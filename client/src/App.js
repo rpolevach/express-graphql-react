3096bb923a209b1d8f7b85037c4b957e1f7073ec
Roman <ayoz4@yandex.ru>
2019-10-03 23:38:10 +0700

    login added

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,51 @@ import Auth from "./components/Auth";
 import Booking from "./components/Booking";
 import Event from "./components/Event";
 import MainNavigation from "./components/Navigation/MainNavigation";
+import AuthContext from "./components/context/authContext";
 import "./App.css";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainNavigation></MainNavigation>
-      <main className="main-content">
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={Auth} />
-          <Route path="/events" component={Event} />
-          <Route path="/bookings" component={Booking} />
-        </Switch>
-      </main>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            token: this.state.token,
+            userId: this.state.userId,
+            login: this.login,
+            logout: this.logout
+          }}
+        >
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              {!this.state.token && <Redirect from="/" to="/auth" exact />}
+              {this.state.token && <Redirect from="/" to="/events" exact />}
+              {this.state.token && <Redirect from="/auth" to="/events" exact />}
+              {!this.state.token && <Route path="/auth" component={Auth} />}
+              <Route path="/events" component={Event} />
+              {this.state.token && (
+                <Route path="/bookings" component={Booking} />
+              )}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Navigation/MainNavigation.js
+++ client/src/components/Navigation/MainNavigation.js
@@ -2,27 +2,38 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import "./MainNavigation.css";
+import AuthContext from "../context/authContext";
 
 const MainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>GraphQL Test</h1>
-    </div>
+  <AuthContext.Consumer>
+    {context => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>GraphQL Test</h1>
+          </div>
 
-    <div className="main-navigation__items">
-      <ul>
-        <li>
-          <NavLink to="/auth">Authenticate</NavLink>
-        </li>
-        <li>
-          <NavLink to="/events">Events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bookings">Bookings</NavLink>
-        </li>
-      </ul>
-    </div>
-  </header>
+          <div className="main-navigation__items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authenticate</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </div>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
 export default MainNavigation;
